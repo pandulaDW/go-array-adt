f6d03a67bcf324676c12735cc2f56a9ae2370cb6
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-10-20 12:25:08 +0530

    Added binary search method

--- arrops/searching.go
+++ arrops/searching.go
@@ -1,6 +1,7 @@
 package arrops
 
 // LinearSearch uses linear search algorithm to obtain the given index of the value
+//
 // Returns -1 if not found
 func (arr *ArrayADT) LinearSearch(value int) int {
 	for i := 0; i < arr.length; i++ {
@@ -17,4 +18,32 @@ func (arr *ArrayADT) Contains(value int) bool {
 		return true
 	}
 	return false
+}
+
+// BinarySearch uses binary search algorithm to obtain the given index of the value.
+//
+// Returns -1 if not found.
+//
+// Slice needs to be sorted to perform this.
+func (arr *ArrayADT) BinarySearch(value int) int {
+	low := 0
+	high := arr.length
+	mid := (low + high) / 2
+
+	for true {
+		if arr.data[mid] == value {
+			return mid
+		}
+		if value < arr.data[mid] {
+			high = mid - 1
+		}
+		if value > arr.data[mid] {
+			low = mid + 1
+		}
+		if low > high || low == arr.length {
+			break
+		}
+		mid = (low + high) / 2
+	}
+	return -1
 }
--- main.go
+++ main.go
@@ -9,13 +9,8 @@ import (
 func main() {
 	a, _ := arrops.CreateArrayADT(0, 10)
 
-	a.Push(1, 2, 3, 4, 5, 6, 7, 8, 9)
+	a.Push(2, 4, 5, 7, 9, 13, 17, 19, 25)
 	a.PrintData()
 
-	fmt.Println(a.LinearSearch(5))  // 2
-	fmt.Println(a.LinearSearch(99)) // -1
-	fmt.Println(a.Contains(91))
-
-	a.Delete(2)
-	a.PrintData()
+	fmt.Println(a.BinarySearch(1))
 }
